6f6a95de2901147a2791fa2c0c76e8b89dba446e
spacebackz <decodingstockmarket@gmail.com>
2025-08-07 22:07:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const puppeteer = require("puppeteer");
+const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage", // This is a common requirement on cloud platforms
+  ],
+  executablePath: "/usr/bin/google-chrome-stable", // Or '/usr/bin/chromium-browser'
+});
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 (async () => {
